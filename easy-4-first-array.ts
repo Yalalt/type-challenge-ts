4ba6d2ac78ba23c254d91462ca2f74451e025d2d
Yalalt <yalaltgandush@gmail.com>
2023-07-02 11:32:17 +0800

    Easy - Parameters type

---
+++ easy-4-first-array.ts
@@ -0,0 +1,29 @@
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+type arr3 = [3];
+type arr4 = [];
+
+// Test solve 1
+type First<T extends any[]> = T extends (string | number)[] ? T[0] : never;
+
+// Test solve 2
+type First2<T extends (string | number)[]> = T extends [infer P, ...(string | number)[]] ? P : never;
+
+// test solve 3
+type First3<T extends any[]> = T['length'] extends 0 ? never : T[0];
+
+// test solve 4 Empty array
+type EmptyArrayFirst<T extends any[]> = T extends [] ? never : T[0];
+
+type head1 = First<arr1> // expected to be "a"
+type head2 = First<arr2> // expected to be 3
+
+type head3 = First2<arr1> // expected to be "a"
+type head4 = First2<arr2> // expected to be 3
+
+type head5 = First3<arr1> // expected to be "a"
+type head6 = First3<arr3> // expected to be 3
+
+type head7 = EmptyArrayFirst<arr4> // expected to be never
+type head8 = EmptyArrayFirst<arr3> // expected to be 3
+type head9 = EmptyArrayFirst<arr1> // expected to be "a"
